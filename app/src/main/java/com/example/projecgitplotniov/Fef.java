4a7e4659ca1991dc1fb0fd22cb35e3f6323d1b62
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2023-03-10 13:21:01 +0700

    Fef class updated

--- app/src/main/java/com/example/projecgitplotniov/Fef.java
+++ app/src/main/java/com/example/projecgitplotniov/Fef.java
@@ -1,2 +1,8 @@
-package com.example.projecgitplotniov;public class Fef {
+package com.example.projecgitplotniov;
+
+public class Fef {
+    public static int ay;
+    public static void cry() {
+
+    }
 }
